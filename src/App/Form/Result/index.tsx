04068070d6f22418a6c5c4bb0611e0eeb5a393a9
Adrian Markefka <am97veb@gmail.com>
2025-09-11 17:43:30 +0200

    migrate Result component to TypeScript

---
+++ src/App/Form/Result/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { ResultProps } from "../../types";
+import { Button, ResultText } from "./styled";
+
+const Result = ({amountValue, result, currencyName, calculation, resultReset,}: ResultProps) => {
+
+  useEffect(() => {
+    resultReset();
+  }, [amountValue]);
+
+  return (
+    <>
+      <p>
+        Wynik:
+        <ResultText>
+          {result === null ? (
+            "brak"
+          ) : (
+            <>
+              {result.calculationResult.toFixed(2)}&nbsp;
+              {result.currencyName}
+            </>
+          )}
+        </ResultText>
+      </p>
+
+      <Button
+        onClick={() =>
+          amountValue === "" || amountValue < "1"
+            ? result === null
+            : calculation(currencyName, amountValue)
+        }
+      >
+        Przelicz
+      </Button>
+    </>
+  );
+};
+export default Result;
