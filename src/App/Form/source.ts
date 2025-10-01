aad4ea0a9a29c28d098ae6a84834836f326b8590
Adrian Markefka <am97veb@gmail.com>
2025-10-01 16:02:13 +0200

    source of exchange rates has been changed

--- src/App/Form/source.ts
+++ src/App/Form/source.ts
@@ -1,3 +1,1 @@
-export const source = "currency-converter-react/currency.json";
-
-// "https://api.currencyapi.com/v3/latest?apikey=cur_live_QMcSRMkZ96pW8YABpy0Cnu6cfxFn560cnkq4pEpW&base_currency=PLN";
+export const source = "https://api.currencyapi.com/v3/latest?apikey=cur_live_QMcSRMkZ96pW8YABpy0Cnu6cfxFn560cnkq4pEpW&base_currency=PLN";
